3acf682705e1b96be0acc88d0d25e11b9e1433e4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 22:34:34 +0430

    Implementing simple event loop

--- pc_async/chapter_01/1_7.py
+++ pc_async/chapter_01/1_7.py
@@ -3,3 +3,14 @@
 """
 
 from collections import deque
+
+
+# Tasks
+tasks = deque()
+tasks.append("hello")
+tasks.append("world")
+
+while True:     # infinite loop
+    if tasks:
+        task = tasks.pop()
+        print('Task:', task)
