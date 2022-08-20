dc7f115be0c08df8da0bb36e4ff5c80699576537
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:10:18 +0430

    Implementing multi-threading section

--- pc_async/chapter_01/1_3.py
+++ pc_async/chapter_01/1_3.py
@@ -11,3 +11,15 @@ def hello_from_thread() -> None:
     :return: None
     """
     print(f"Hello from thread {threading.current_thread()}!")
+
+
+# Multi-threading section
+hello_thread = threading.Thread(target=hello_from_thread)
+hello_thread.start()
+
+total_threads = threading.active_count()
+thread_name = threading.current_thread().name
+
+print(f"Python is currently running {total_threads} thread(s).")
+print(f"The current thread is {thread_name}.")
+hello_thread.join()
