931241817f3500a9c9d06a5e6fe9361cb2e053ee
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:01:48 +0430

    Defining hello_from_thread function

--- pc_async/chapter_01/1_3.py
+++ pc_async/chapter_01/1_3.py
@@ -3,3 +3,11 @@
 """
 
 import threading
+
+
+def hello_from_thread() -> None:
+    """
+    Print hello from thread function
+    :return: None
+    """
+    print(f"Hello from thread {threading.current_thread()}!")
