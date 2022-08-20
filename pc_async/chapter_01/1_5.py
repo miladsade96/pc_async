2e2086dd544bd0ce7a00386937411f970c3d4cc7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:35:09 +0430

    Implementing no-threading section

--- pc_async/chapter_01/1_5.py
+++ pc_async/chapter_01/1_5.py
@@ -19,3 +19,16 @@ def print_fib(number: int) -> None:
         else:
             return fib(n - 1) + fib(n - 2)
     print(f"fib({number}) is {fib(number)}")
+
+
+# No-threading Section
+def fibs_no_threading():
+    print_fib(40)
+    print_fib(41)
+
+
+start = time.time()
+fibs_no_threading()
+end = time.time()
+
+print(f"Completed in {end - start:.4f} seconds.")
