20687ea81d1cd70260175944f99a8fd273f5d73a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:16:40 +0430

    Defining hello_from_process function

--- pc_async/chapter_01/1_4.py
+++ pc_async/chapter_01/1_4.py
@@ -4,3 +4,11 @@
 
 import os
 import multiprocessing
+
+
+def hello_from_process() -> None:
+    """
+    Print hello from process
+    :return: None
+    """
+    print(f"Hello from child process {os.getpid()}!")
