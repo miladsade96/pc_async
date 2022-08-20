74fee758dc7c33ccb77bc13421b87a770a838c99
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:34:37 +0430

    Implementing print_fib function

--- pc_async/chapter_01/1_5.py
+++ pc_async/chapter_01/1_5.py
@@ -3,3 +3,19 @@
 """
 
 import time
+
+
+def print_fib(number: int) -> None:
+    def fib(n: int) -> int:
+        """
+        Calculating fibonacci sequence
+        :param n: fibonacci sequence from 1 to n
+        :return: nth fibonacci number
+        """
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            return fib(n - 1) + fib(n - 2)
+    print(f"fib({number}) is {fib(number)}")
