62b9a45dc5776d1b908d2090bc736016afac311a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 18:17:23 +0430

    Implementing multi-processing section

--- pc_async/chapter_01/1_4.py
+++ pc_async/chapter_01/1_4.py
@@ -12,3 +12,11 @@ def hello_from_process() -> None:
     :return: None
     """
     print(f"Hello from child process {os.getpid()}!")
+
+
+# Multi-Processing section
+if __name__ == '__main__':
+    hello_process = multiprocessing.Process(target=hello_from_process)
+    hello_process.start()
+    print(f"Hello from parent process {os.getpid()}!")
+    hello_process.join()
