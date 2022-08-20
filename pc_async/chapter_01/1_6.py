97c65522b34914578a8c261af947b7b0785840cd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 19:08:05 +0430

    Defining read_status function

--- pc_async/chapter_01/1_6.py
+++ pc_async/chapter_01/1_6.py
@@ -4,3 +4,12 @@
 
 import time
 import requests
+
+
+def read_status() -> None:
+    """
+    Read the website status code.
+    :return: None
+    """
+    response = requests.get("https://elns.info")
+    print(response.status_code)
