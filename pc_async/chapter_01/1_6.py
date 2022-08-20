6b898abe9d60412d448c9ae6bc7c56ecf7bc5f80
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 19:17:37 +0430

    Implementing multi-threading section

--- pc_async/chapter_01/1_6.py
+++ pc_async/chapter_01/1_6.py
@@ -4,6 +4,7 @@
 
 import time
 import requests
+import threading
 
 
 def read_status() -> None:
@@ -28,3 +29,31 @@ sync_end = time.time()
 print(f"running synchronously took {sync_end - sync_start:.4f} seconds.")
 
 # ----------------------------------------------------------------------------------
+
+# Multi-threading section
+thread_1 = threading.Thread(target=read_status)
+thread_2 = threading.Thread(target=read_status)
+thread_3 = threading.Thread(target=read_status)
+thread_4 = threading.Thread(target=read_status)
+thread_5 = threading.Thread(target=read_status)
+
+
+thread_start = time.time()
+
+thread_1.start()
+thread_2.start()
+thread_3.start()
+thread_4.start()
+thread_5.start()
+
+print("All threads are running!")
+
+thread_1.join()
+thread_2.join()
+thread_3.join()
+thread_4.join()
+thread_5.join()
+
+thread_end = time.time()
+
+print(f"Running with threads took {thread_end - thread_start:.4f} seconds.")
