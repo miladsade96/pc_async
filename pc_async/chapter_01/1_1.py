4f190564cff3e186d6e786c0ee818fe8d6011a84
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-20 15:03:27 +0430

    Implementing I/O-bound and CPU-bound operations

--- pc_async/chapter_01/1_1.py
+++ pc_async/chapter_01/1_1.py
@@ -3,3 +3,11 @@
 """
 
 import requests
+
+response = requests.get('https://www.elns.info')    # I/O-bound web request
+items = response.headers.items()
+headers = [f"{key}: {header}" for key, header in items]   # CPU-bound response processing
+formatted_headers = '\n'.join(headers)  # CPU-bound string concatenation
+
+with open('1_1.txt', 'w') as f:
+    f.write(formatted_headers)
