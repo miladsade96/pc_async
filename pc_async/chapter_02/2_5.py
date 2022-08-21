47d5c8670004969516e75c22ef21d598209fc46a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-21 19:09:52 +0430

    Using asyncio.sleep

--- pc_async/chapter_02/2_5.py
+++ pc_async/chapter_02/2_5.py
@@ -2,3 +2,24 @@
     First application with sleep
 """
 import asyncio
+
+
+async def hello_world_message() -> str:
+    """
+    Return hello world message
+    :return: hello world message
+    """
+    await asyncio.sleep(1)      # pause for 1 second
+    return "Hello World"
+
+
+async def main() -> None:
+    """
+    Main function
+    :return: None
+    """
+    message = await hello_world_message()   # pause main until hello_world_message is done
+    print(message)
+
+
+asyncio.run(main())
