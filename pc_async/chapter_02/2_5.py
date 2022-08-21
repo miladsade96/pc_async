e75b320cbaad43b93b442bbba37fee915b3e2bbe
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-21 19:44:51 +0430

    Implementing delay coroutine

--- pc_async/chapter_02/2_5.py
+++ pc_async/chapter_02/2_5.py
@@ -4,6 +4,18 @@
 import asyncio
 
 
+async def delay(delay_seconds: int) -> int:
+    """
+    Delay for delay_seconds seconds
+    :param delay_seconds: number of seconds to delay
+    :return: number of seconds delayed
+    """
+    print(f"Sleeping for {delay_seconds} seconds.")
+    await asyncio.sleep(delay_seconds)
+    print(f"Done sleeping for {delay_seconds} seconds.")
+    return delay_seconds
+
+
 async def hello_world_message() -> str:
     """
     Return hello world message
