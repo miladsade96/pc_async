0a63b68457e689fdf40a699100a9a2b34eb90eb4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-21 18:51:49 +0430

    Defining and running a coroutine

--- pc_async/chapter_02/2_3.py
+++ pc_async/chapter_02/2_3.py
@@ -3,3 +3,18 @@
 """
 
 import asyncio
+
+
+# Defining a coroutine
+async def coroutine_add_one(number: int) -> int:
+    """
+    Add one to the number
+    :param number: int
+    :return: int
+    """
+    return number + 1
+
+
+# Running a coroutine
+result = asyncio.run(coroutine_add_one(1))
+print(result)
