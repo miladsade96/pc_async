bc953b799d19a1781e71187551f3b089052fa4fc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-21 18:46:44 +0430

    Implementing comparison section

--- pc_async/chapter_02/2_2.py
+++ pc_async/chapter_02/2_2.py
@@ -19,3 +19,11 @@ def normal_add_one(number: int) -> int:
     :return: int
     """
     return number + 1
+
+
+# Comparison section
+function_result = normal_add_one(1)
+coroutine_result = coroutine_add_one(1)
+
+print(f"Function result is {function_result} and the type is {type(function_result)}")
+print(f"Coroutine result is {coroutine_result} and the type is {type(coroutine_result)}")
