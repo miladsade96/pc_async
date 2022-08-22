2594bcaf8d1d7f464105359a5ec141ce915256c7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 14:56:56 +0430

    Creating a task

--- pc_async/chapter_02/2_7.py
+++ pc_async/chapter_02/2_7.py
@@ -4,3 +4,12 @@
 
 import asyncio
 from pc_async.utils.delay_functions import delay
+
+
+async def main():
+    sleep_for_three = asyncio.create_task(delay(3))
+    print(f"type(sleep_for_three) = {type(sleep_for_three)}")
+    result = await sleep_for_three
+    print(f"result = {result}")
+
+asyncio.run(main())
