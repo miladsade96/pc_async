c595dd62243f77ee7d79bf147077959615222928
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-22 15:06:33 +0430

    Running multiple tasks concurrently

--- pc_async/chapter_02/2_8.py
+++ pc_async/chapter_02/2_8.py
@@ -4,3 +4,16 @@
 
 import asyncio
 from pc_async.utils.delay_functions import delay
+
+
+async def main():
+    sleep_for_three = asyncio.create_task(delay(10))
+    sleep_again = asyncio.create_task(delay(5))
+    sleep_one_more = asyncio.create_task(delay(3))
+
+    await sleep_for_three
+    await sleep_again
+    await sleep_one_more
+
+
+asyncio.run(main())
