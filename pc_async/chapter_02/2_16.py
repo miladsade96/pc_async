73120cee7927b2960bc745b7b3b22a282ecc0b67
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 13:00:14 +0430

    Timing two concurrent tasks with a decorator

--- pc_async/chapter_02/2_16.py
+++ pc_async/chapter_02/2_16.py
@@ -4,3 +4,23 @@
 
 import asyncio
 from pc_async.utils.decorators import async_timed
+
+
+@async_timed()
+async def delay(delay_seconds: int) -> int:
+    print(f"Sleeping for {delay_seconds} seconds.")
+    await asyncio.sleep(delay_seconds)
+    print(f"Finished sleeping for {delay_seconds} seconds.")
+    return delay_seconds
+
+
+@async_timed()
+async def main() -> None:
+    task_1 = asyncio.create_task(delay(2))
+    task_2 = asyncio.create_task(delay(3))
+
+    await task_1
+    await task_2
+
+
+asyncio.run(main())
