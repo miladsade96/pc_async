1aa634f349c3352aa85912fc6478a3af6b54db4f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 12:49:53 +0430

    Implementing async_timed decorator

--- pc_async/chapter_02/2_15.py
+++ pc_async/chapter_02/2_15.py
@@ -5,3 +5,19 @@
 from time import time
 from functools import wraps
 from typing import Any, Callable
+
+
+def async_timed():
+    def wrapper(func: Callable) -> Callable:
+        @wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            print(f"Starting {func} with args {args} {kwargs}")
+            start = time()
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                end = time()
+                total = end - start
+                print(f"Finished {func} in {total:.4f} second(s)")
+        return wrapped
+    return wrapper
