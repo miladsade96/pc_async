4856561aa19e2f3da2c09a35741274124667e68f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 11:36:53 +0430

    The basics of futures

--- pc_async/chapter_02/2_13.py
+++ pc_async/chapter_02/2_13.py
@@ -3,3 +3,11 @@
 """
 
 from asyncio import Future
+
+
+my_future = Future()    # Defining a future
+print(f"Is my_future done? {my_future.done()}")
+
+my_future.set_result(42)    # Setting the result
+print(f"Is my_future done? {my_future.done()}")
+print(f"What is the result of my_future? {my_future.result()}")
