1b8492e63f1e918beb2740729f40a80a8eec1601
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 13:13:42 +0430

    Attempting to run CPU-bound code concurrently

--- pc_async/chapter_02/2_17.py
+++ pc_async/chapter_02/2_17.py
@@ -5,3 +5,23 @@
 import asyncio
 from pc_async.utils.delay_functions import delay
 from pc_async.utils.decorators import async_timed
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(100000000):
+        counter += 1
+    return counter
+
+
+@async_timed()
+async def main() -> None:
+    task_1 = asyncio.create_task(cpu_bound_work())
+    task_2 = asyncio.create_task(cpu_bound_work())
+
+    await task_1
+    await task_2
+
+
+asyncio.run(main())
