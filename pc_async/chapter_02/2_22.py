b8e27aaef0fa4c3a9dc9ca614e3b7104a6efcf1c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 14:18:37 +0430

    Running CPU-bound code in debug mode

---
+++ pc_async/chapter_02/2_22.py
@@ -0,0 +1,22 @@
+"""
+    Running CPU-bound code in debug mode
+"""
+
+import asyncio
+from pc_async.utils.decorators import async_timed
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(100000000):
+        counter = counter + 1
+    return counter
+
+
+async def main() -> None:
+    task_one = asyncio.create_task(cpu_bound_work())
+    await task_one
+
+
+asyncio.run(main(), debug=True)
