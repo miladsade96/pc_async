12e08c12a21684c26f48a16f1f2c5e1bd6f7cb0c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 11:09:57 +0430

    Creating a timeout for a task with wait_for

--- pc_async/chapter_02/2_11.py
+++ pc_async/chapter_02/2_11.py
@@ -4,3 +4,16 @@
 
 import asyncio
 from pc_async.utils.delay_functions import delay
+
+
+async def main() -> None:
+    delay_task = asyncio.create_task(delay(3))
+    try:
+        result = await asyncio.wait_for(delay_task, timeout=1)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print("Got a timeout")
+        print(f"Was the task cancelled? {delay_task.cancelled()}")
+
+
+asyncio.run(main())
