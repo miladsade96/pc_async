ccf3d5f07f71c2f2f9a1b87925b1db870c7892e6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 11:25:52 +0430

    Shielding a task from cancellation

--- pc_async/chapter_02/2_12.py
+++ pc_async/chapter_02/2_12.py
@@ -4,3 +4,18 @@
 
 import asyncio
 from pc_async.utils.delay_functions import delay
+
+
+async def main() -> None:
+    task = asyncio.create_task(delay(10))
+
+    try:
+        result = await asyncio.wait_for(asyncio.shield(task), timeout=5)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print("Task took longer than 5 seconds, It will finish soon.")
+        result = await task
+        print(result)
+
+
+asyncio.run(main())
