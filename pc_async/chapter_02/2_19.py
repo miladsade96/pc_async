9d03d2ca465dd338e9963d36b52c0085066e7898
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-24 13:32:06 +0430

    Incorrectly using a blocking API in a coroutine

--- pc_async/chapter_02/2_19.py
+++ pc_async/chapter_02/2_19.py
@@ -5,3 +5,22 @@
 import asyncio
 import requests
 from pc_async.utils.decorators import async_timed
+
+
+@async_timed()
+async def get_google_status() -> int:
+    return requests.get("https://www.google.com").status_code
+
+
+@async_timed()
+async def main() -> None:
+    task_1 = asyncio.create_task(get_google_status())
+    task_2 = asyncio.create_task(get_google_status())
+    task_3 = asyncio.create_task(get_google_status())
+
+    await task_1
+    await task_2
+    await task_3
+
+
+asyncio.run(main())
