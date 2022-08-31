ec00f06733d9a6b08f27ceb87bafb2bc1196a177
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-31 18:12:40 +0430

    Reading data from a socket and write back to the client

--- pc_async/chapter_03/3_2.py
+++ pc_async/chapter_03/3_2.py
@@ -1,5 +1,32 @@
 """
-    Reading data from a socket
+    Reading data from a socket and write back to the client
 """
 
 import socket
+
+# Creating a tcp server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)    # Setting the address of the socket
+server_socket.bind(server_address)
+server_socket.listen()      # Listening for connection
+
+try:
+    connection, client_address = server_socket.accept()
+    print(f"I got a connection from {client_address}")
+
+    buffer = b''
+
+    while buffer[-2:] != b'\r\n':
+        data = connection.recv(2)   # Trying to receive two bytes
+        if not data:
+            break
+        else:
+            print(f"I got data {data}")
+            buffer += data  # Storing in buffer
+
+    print(f"All the data is {buffer}")
+    connection.sendall(buffer)      # Write data back to the client
+finally:
+    server_socket.close()
