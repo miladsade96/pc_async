9c271d283c1ccf8c7ca07f3f21fe62a7276b635f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-09-01 17:18:22 +0430

    Marking the server socket as non-blocking

--- pc_async/chapter_03/3_5.py
+++ pc_async/chapter_03/3_5.py
@@ -3,3 +3,12 @@
 """
 
 import socket
+
+# Creating a tcp server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)    # Setting the address of the socket
+server_socket.bind(server_address)
+server_socket.listen()      # Listening for connection
+server_socket.setblocking(False)    # Marking the server socket as non-blocking
